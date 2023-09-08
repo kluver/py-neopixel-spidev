01c51c6cfc7826aba984cb2343e3bae33aaf257a
Daniel Kluver <kluve018@umn.edu>
2023-09-07 22:46:29 -0500

    Update neopixel_spi_write.py
    Hotfix based on https://github.com/jgarff/rpi_ws281x/issues/469#issuecomment-1507099647 to make this code work on my platform where (apparently) SPI is held high. Sends a 0 byte before data to account for it.

--- lib/neopixel_spi_write.py
+++ lib/neopixel_spi_write.py
@@ -9,8 +9,8 @@ import numpy
 # See: https://github.com/joosteto/ws2812-spi/blob/master/ws2812.py
 def neopixel_spi_write(spi, data):
     d = numpy.array(data).ravel()
-    tx = numpy.zeros(len(d) * 4, dtype = numpy.uint8)
+    tx = numpy.zeros(len(d) * 4+1, dtype = numpy.uint8)
     for ibit in range(4):
-        tx[3 - ibit::4] = ((d >> (2 * ibit + 1)) & 1) * 0x60 + \
+        tx[4 - ibit::4] = ((d >> (2 * ibit + 1)) & 1) * 0x60 + \
                           ((d >> (2 * ibit + 0)) & 1) * 0x06 + 0x88
     spi.xfer(tx.tolist())
